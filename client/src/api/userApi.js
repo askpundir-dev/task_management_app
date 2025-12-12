09f85139a4081705b2556bda5b2a2da30bf4edca
Abhishek Pundir <ask.pundir0224@gmail.com>
2025-12-12 20:13:01 +0530

    fix: update CORS configuration to allow multiple origins and improve security

--- client/src/api/userApi.js
+++ client/src/api/userApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { throwAPIError } from "../utils/apiError";
 
 // ----------------------------------------------
-const API_URL = "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
--- server/server.js
+++ server/server.js
@@ -9,14 +9,22 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+const allowedOrigins = ["http://localhost:5173", "http://localhost:4173"];
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
   })
 );
 
