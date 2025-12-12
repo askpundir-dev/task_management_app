5cc1b1811312053fded8fba0678058a07c0b2148
Abhishek Pundir <ask.pundir0224@gmail.com>
2025-12-12 15:55:25 +0530

    feat: add Header and Loading components; enhance Home page with task management features

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="min-h-screen bg-white">
+      <div className="fixed top-1/2 left-1/2 -translate-1/2 border-3 w-10 h-10 border-red-700 border-b-transparent rounded-[50%] animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loading;
--- client/src/routeComponents/ProtectedRoute.jsx
+++ client/src/routeComponents/ProtectedRoute.jsx
@@ -1,12 +1,13 @@
 import { useAuth } from "../context-provider/AuthContext";
 import { Navigate } from "react-router-dom";
+import Loading from "../components/Loading";
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, loading } = useAuth();
   console.log("isAuthenticated:", isAuthenticated);
   console.log("loading is:", loading);
 
-  if (loading) return <p>LOADING...</p>;
+  if (loading) return <Loading />;
 
   return isAuthenticated ? children : <Navigate to="/login" />;
 };
